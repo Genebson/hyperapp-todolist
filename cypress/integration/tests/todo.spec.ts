e76e92aa6acdb964193cc02371960a471b0e345e
Genebson <mauryavero@gmail.com>
2022-01-06 08:49:28 -0300

    Finish the app tests

--- cypress/integration/tests/todo.spec.ts
+++ cypress/integration/tests/todo.spec.ts
@@ -3,12 +3,12 @@ const URLHost = "http://localhost:8081/";
 describe("Todo List App", () => {
   it("Should add two tasks and press button Add Task", () => {
     cy.visit(URLHost);
-    cy.get("#todo-list-input").type("Do the laundry");
-    cy.get("#todo-list-btn-add").click();
-    cy.get("#todo-list-input").should("be.empty");
-    cy.get("#todo-list-input").type("Do the dishes");
-    cy.get("#todo-list-btn-add").click();
-    cy.get("#todo-list-input").should("be.empty");
+    cy.get("#todo-list-input").as("input").type("Do the laundry");
+    cy.get("#todo-list-btn-add").as("addBtn").click();
+    cy.get("@input").should("be.empty");
+    cy.get("@input").type("Do the dishes");
+    cy.get("@addBtn").click();
+    cy.get("@input").should("be.empty");
   });
 
   it("Should check both tasks and delete them", () => {
@@ -18,18 +18,20 @@ describe("Todo List App", () => {
       });
     });
     cy.get(".todo-list.delete-task-active").then((deleteItems) => {
-      console.log(deleteItems);
-
       deleteItems.each((_, deleteItem) => {
+        deleteItem.click();
+        cy.wait(0);
         deleteItem.click();
       });
     });
   });
 
-  // it("Should add one task and press Enter key", () => {
-  //   cy.get("#todo-list-input").type("Do the laundry{enter}");
-  //   cy.get("#todo-list-input").should("be.empty");
-  //   cy.get("#todo-list-input").type("Do the dishes{enter}");
-  //   cy.get("#todo-list-input").should("be.empty");
-  // });
+  it("Should add three tasks and press Enterkey", () => {
+    cy.get("#todo-list-input").as("input").type("Do the laundry{enter}");
+    cy.get("@input").should("be.empty");
+    cy.get("@input").type("Do the dishes{enter}");
+    cy.get("@input").should("be.empty");
+    cy.get("@input").type("Clean the room{enter}");
+    cy.get("@input").should("be.empty");
+  });
 });
